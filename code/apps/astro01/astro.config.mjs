6a069eed8f1df2aaafbad72494ae3649b7691171
Bibby Chung <bibbynet@gmail.com>
2025-08-23 18:43:04 +0800

    feat: add angular support

--- code/apps/astro01/astro.config.mjs
+++ code/apps/astro01/astro.config.mjs
@@ -3,12 +3,14 @@ import svelte from '@astrojs/svelte'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
+import analogjsangular from '@analogjs/astro-angular';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [svelte()],
+  integrations: [svelte(), analogjsangular()],
   experimental: {
     staticImportMetaEnv: true,
   },
---
+++ code/apps/astro01/src/lib/components/ng/hello-world.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
+import confetti from 'canvas-confetti'
+import { tap } from 'rxjs'
+import { getSubject } from '~/lib/common/util'
+import { EnvComponent } from '~/lib/components/ng/env.component'
+
+@Component({
+  selector: 'app-hello-world',
+  imports: [CommonModule, EnvComponent],
+  template: `
+    <h2 class="p-2 font-serif text-2xl text-black">hello world - {{ lang }}</h2>
+    <button class="btn mt-2" (click)="btnClick$.next($event)">click it</button>
+    <hr />
+    <app-env />
+  `,
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HelloWorldComponent {
+  lang = 'svelte'
+
+  btnClick$ = getSubject<Event>()
+
+  bntClickSub = this.btnClick$
+    .pipe(
+      tap((e) => {
+        e.preventDefault()
+        confetti()
+      }),
+      takeUntilDestroyed(),
+    )
+    .subscribe()
+}
