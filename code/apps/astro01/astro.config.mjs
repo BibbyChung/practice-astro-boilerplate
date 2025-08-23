e4f008025479bc4eb2bd9efb431746b045ca15b9
Bibby Chung <bibbynet@gmail.com>
2025-08-24 02:01:18 +0800

    feat: add vue support
    # Conflicts:
    #	code/apps/astro01/astro.config.mjs
    
    # Conflicts:
    #	code/apps/astro01/astro.config.mjs
    #	code/apps/astro01/package.json
    #	pnpm-lock.yaml

--- code/apps/astro01/astro.config.mjs
+++ code/apps/astro01/astro.config.mjs
@@ -2,12 +2,14 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
+import vue from '@astrojs/vue'
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [],
+  integrations: [vue()],
   build: {
     assets: 'assets',
   },
