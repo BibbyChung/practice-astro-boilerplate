8e18aa543ed454d7e401b57f6ea16baf41f58cb4
Bibby Chung <bibbynet@gmail.com>
2025-08-24 16:52:44 +0800

    feat: add react support
    # Conflicts:
    #	code/apps/astro01/astro.config.mjs
    
    # Conflicts:
    #	code/apps/astro01/astro.config.mjs
    #	code/apps/astro01/package.json
    #	pnpm-lock.yaml

--- code/apps/astro01/astro.config.mjs
+++ code/apps/astro01/astro.config.mjs
@@ -2,12 +2,14 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
+import react from '@astrojs/react'
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [],
+  integrations: [react()],
   build: {
     assets: 'assets',
   },
