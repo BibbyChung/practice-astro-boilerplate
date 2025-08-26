4c111c75b2da992da1f013c42186b7be40dae9f7
Bibby Chung <bibbynet@gmail.com>
2025-08-27 02:05:42 +0800

    feat: add bash script to package.json
    # Conflicts:
    #	code/apps/astro01/package.json

---
+++ code/apps/astro01/.bin/tools.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+set -ue #xv
+set -o pipefail
+
+_currentFolder="$(realpath $0 | sed 's|\(.*\)/.*|\1|')/"
+
+_func=${1:-""}
+_nodeEnv=${2:-""}
+
+# =============== utilies ===============
+
+function log() {
+  echo "===============> $1 "
+}
+
+function loadEnv() {
+  if [ -f "${_currentFolder}../.env" ]; then
+    log "load .env"
+    # export $(cat "${_currentFolder}../.env" | xargs)
+    source "${_currentFolder}../.env"
+  fi
+}
+
+# =============== action ===============
+
+if [[ "${_func}" == "runDev" ]]; then
+  # pnpm generate:env:dev
+  # pnpm generate:env:more
+  loadEnv
+  log "astro dev --host=0.0.0.0 --port=3000"
+  astro dev --host=0.0.0.0 --port=3000
+fi
+
+if [[ "${_func}" == "runProd" ]]; then
+  loadEnv
+  log "run prod"
+  HOST=0.0.0.0 PORT=3000 node ${_currentFolder}../dist/server/entry.mjs
+fi
+
+if [[ "${_func}" == "build" ]]; then
+  log "rm dist"
+  rm -rf ${_currentFolder}../dist
+
+  # pnpm generate:env:prod
+  # loadEnv
+  # log "build cjs"
+  # tsc -p ${_currentFolder}../tsconfig.commonjs.json
+  # pnpm generate:env:more
+  astro build
+fi
+
+# =============== (deprecated) ===============
+
+# supoort evn.json env.prod.json .env.dev.json
+if [[ "${_func}" == "createEnv" ]]; then
+  log "createEnv"
+  __envPath=""
+  if [[ "${_nodeEnv}" == "prod" ]]; then
+    __envPath=${_currentFolder}../.env.prod.json
+  else
+    __envPath=${_currentFolder}../.env.dev.json
+  fi
+  log "create .env.json (${_nodeEnv})"
+  cat ${__envPath} >${_currentFolder}../src/_env.json
+  log "create .env (${_nodeEnv})"
+  node ${_currentFolder}../../../.bin/convertJsonToEnv.js ${__envPath} ${_currentFolder}../.env
+  echo "NODE_ENV=\"${_nodeEnv}\"" >>${_currentFolder}../.env
+  echo "VITE_NODE_ENV=\"${_nodeEnv}\"" >>${_currentFolder}../.env
+fi
+
+if [[ "${_func}" == "createEnvMore" ]]; then
+  __envPath=${_currentFolder}../.env
+  __isExistCount=$(cat ${__envPath} | { grep "GIT_SHORT_VER" || :; } | wc -l)
+  if ((${__isExistCount} != 0)); then
+    log "env more info existed"
+    exit 0
+  fi
+  log "add env more info"
+  echo "VITE_GIT_SHORT_VER=\"${_gitShortVer:-"xxxx"}\"" >>${__envPath}
+  echo "VITE_GIT_TIME=\"${_gitTime:-"xxxx"}\"" >>${__envPath}
+fi
+
+if [[ "${_func}" == "" ]]; then
+  echo "please add args..."
+fi
