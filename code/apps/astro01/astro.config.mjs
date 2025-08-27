4bfa3f9bc9b6754ca4d19eb38094ac0ab0f2031c
Bibby Chung <bibbynet@gmail.com>
2025-08-27 23:28:20 +0800

    feat: add svelte support

--- code/apps/astro01/astro.config.mjs
+++ code/apps/astro01/astro.config.mjs
@@ -2,12 +2,14 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
+import svelte from '@astrojs/svelte'
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [],
+  integrations: [svelte()],
   build: {
     assets: 'assets',
   },
