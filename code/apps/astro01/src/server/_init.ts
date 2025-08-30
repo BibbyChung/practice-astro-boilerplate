48ed92353fecb57db100a2ed4598a2aa736e62c8
Bibby Chung <bibbynet@gmail.com>
2025-08-31 01:57:04 +0800

    fix: fix errors
    # Conflicts:
    #	code/apps/astro01/src/lib/components/menu.astro

--- code/apps/astro01/src/server/_init.ts
+++ code/apps/astro01/src/server/_init.ts
@@ -1,6 +1,5 @@
 import type { ZodType, z } from 'zod'
 import { trpcContext, type Context } from './_context'
-import { todosRouter } from './todos.router'
 import { userRouter } from './user.router'
 
 export type AppRouterType = typeof appRouter
@@ -12,7 +11,6 @@ export type HandleOptsType<T extends ZodType> = {
 
 export const appRouter = trpcContext.router({
   user: trpcContext.router(userRouter),
-  todos: trpcContext.router(todosRouter),
 })
 
 export const appCaller = trpcContext.createCallerFactory(appRouter)
