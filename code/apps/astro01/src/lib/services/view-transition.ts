2db5a02525c7f0895b3e4e9b473c00e078e17e88
Bibby Chung <bibbynet@gmail.com>
2025-09-02 03:53:01 +0800

    fix: fix event errors

--- code/apps/astro01/src/lib/services/view-transition.ts
+++ code/apps/astro01/src/lib/services/view-transition.ts
@@ -1,25 +1,33 @@
-import { tap } from 'rxjs'
+import { fromEvent, switchMap, tap } from 'rxjs'
 import { getWindow } from './layout.service'
 
-export const mountVTSwap = (func: () => void) => {
+export const mountVTSwap = (pathname: string, func: () => void) => {
   getWindow()
     .pipe(
-      tap((w) => {
-        w.document.addEventListener('astro:after-swap', () => {
+      switchMap((w) => fromEvent(w.document, 'astro:page-load')),
+      tap((e) => {
+        const d = e.target as Document
+        const currentPathname = d.location.pathname
+        // console.log(`mountVTSwap currentPathname: ${currentPathname}`)
+        if (currentPathname === pathname || pathname === '*') {
           func()
-        })
+        }
       }),
     )
     .subscribe()
 }
 
-export const unmountVTSwap = (func: () => void) => {
+export const unmountVTSwap = (pathname: string, func: () => void) => {
   getWindow()
     .pipe(
-      tap((w) => {
-        w.document.addEventListener('astro:before-swap', () => {
+      switchMap((w) => fromEvent(w.document, 'astro:before-swap')),
+      tap((e) => {
+        const d = e.target as Document
+        const currentPathname = d.location.pathname
+        // console.log(`unmountVTSwap currentPathname: ${currentPathname}`)
+        if (currentPathname === pathname || pathname === '*') {
           func()
-        })
+        }
       }),
     )
     .subscribe()
